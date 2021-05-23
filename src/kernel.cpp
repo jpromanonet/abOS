81c0fe531884bb1cb5b394a27bc0f2aa9d3db5af
Juan P. Romano <programming@jpromano.net>
2021-05-23 20:27:11 -0300

    Defining the kernel main function

---
+++ src/kernel.cpp
@@ -0,0 +1,10 @@
+// We start by declaring the main kernel function of abOS
+
+void kernelMain()
+{
+    // Just a test message
+    printf("Hellow World! | https://juanpr.com");
+
+    // We use a infinit loop becase we don't want our kernel to stop
+    while(1);
+}
