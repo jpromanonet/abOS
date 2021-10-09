2a7e1274a1437684526def6c589e686972d0d790
jpromanonet <programming@jpromano.net>
2021-10-09 16:39:39 -0300

    Loader done

--- src/loader.s
+++ src/loader.s
@@ -1,6 +1,12 @@
 # Here we tell our bootloader that the OS existe and to fire it up.
 .set MAGIC, 0x1badb002
 .set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
 
 .section .text
 .extern kernelMain
