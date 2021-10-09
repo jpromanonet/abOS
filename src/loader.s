542051e3a3ba65714529fe85e9f9fd14bad66b5b
jpromanonet <programming@jpromano.net>
2021-10-09 16:38:00 -0300

    Loader

--- src/loader.s
+++ src/loader.s
@@ -1,10 +1,19 @@
+# Here we tell our bootloader that the OS existe and to fire it up.
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+
 .section .text
 .extern kernelMain
 .global loader
 
 loader:
-    mov $kernel_stack, %esp
+    mov $kernel_stack, %esp+
+    call kernelMain
 
+_stop:
+    cli
+    htl
+    jmp _stop
 
 .section .bss
 .space 2*1024*1024; #2 Mib
